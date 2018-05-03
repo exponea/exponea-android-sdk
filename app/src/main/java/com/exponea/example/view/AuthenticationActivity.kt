b5b0a9d6b8dcc9ee3dac08873af9e8cdef980efa
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-03 10:54:44 +0200

    Auth activity setup

---
+++ app/src/main/java/com/exponea/example/view/AuthenticationActivity.kt
@@ -0,0 +1,13 @@
+package com.exponea.example.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.exponea.example.R
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}

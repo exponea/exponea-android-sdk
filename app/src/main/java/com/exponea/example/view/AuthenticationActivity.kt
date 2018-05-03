d23e0131250479031706a87809a8d9ac71580364
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-03 13:39:17 +0200

    edit text listeners added
    extension moved to separate package

--- app/src/main/java/com/exponea/example/view/AuthenticationActivity.kt
+++ app/src/main/java/com/exponea/example/view/AuthenticationActivity.kt
@@ -2,12 +2,34 @@ package com.exponea.example.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.exponea.example.R
+import com.exponea.example.utils.isValid
+import com.exponea.example.utils.onTextChanged
+import kotlinx.android.synthetic.main.activity_authentication.*
 
 class AuthenticationActivity : AppCompatActivity() {
+
+    var authCode = ""
+    var projectToken = ""
+    var customerIds = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+        editTextAuthCode.onTextChanged { authCode = it  }
+        editTextCustomersIds.onTextChanged { customerIds = it }
+        editTextProjectToken.onTextChanged { projectToken = it }
+
+        button.setOnClickListener {
+            if (!editTextProjectToken.isValid() || !editTextCustomersIds.isValid()
+                    || !editTextAuthCode.isValid()) {
+                Toast.makeText(this, "Empty field", Toast.LENGTH_SHORT).show()
+            } else {
+                // login
+            }
+        }
+
     }
 }

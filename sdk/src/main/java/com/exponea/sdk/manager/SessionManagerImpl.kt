73091da50bec5de794c6c79cead4da4758865a49
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-07 11:11:12 +0200

    track session end and it's duration

--- sdk/src/main/java/com/exponea/sdk/manager/SessionManagerImpl.kt
+++ sdk/src/main/java/com/exponea/sdk/manager/SessionManagerImpl.kt
@@ -16,6 +16,11 @@ class SessionManagerImpl(val prefs: ExponeaPreferences) : SessionManager {
         const val PREF_SESSION_START = "SessionStartTime"
     }
 
+    private fun getSessionLengthInSeconds() : Long {
+        val start = prefs.getLong(PREF_SESSION_START, Date().time)
+        val end = prefs.getLong(PREF_SESSION_END, Date().time)
+        return (end - start) / 1000
+    }
 
     override fun onSessionStart() {
         Logger.d(this, "Session start")
@@ -34,6 +39,7 @@ class SessionManagerImpl(val prefs: ExponeaPreferences) : SessionManager {
         val timestamp = Date().time
         Logger.d(this, "Now Ended: ${Date(timestamp)}")
         prefs.setLong(PREF_SESSION_END, timestamp)
+        trackEnd(timestamp)
 
     }
 
@@ -42,11 +48,25 @@ class SessionManagerImpl(val prefs: ExponeaPreferences) : SessionManager {
         val properties = DeviceProperties().toHashMap()
         properties["app_version"] = BuildConfig.VERSION_CODE
         Exponea.trackEvent(
-                eventType = Constants.EventTypes.installation,
+                eventType = Constants.EventTypes.sessionStart,
                 timestamp = timestamp,
                 properties = properties,
                 route = Route.TRACK_EVENTS
         )
+    }
+
+    private fun trackEnd(timestamp: Long) {
+        val properties = DeviceProperties().toHashMap()
+        properties["app_version"] = BuildConfig.VERSION_CODE
+        properties["duration"] = getSessionLengthInSeconds()
+        Exponea.trackEvent(
+                eventType = Constants.EventTypes.sessionEnd,
+                timestamp = timestamp,
+                properties = properties,
+                route = Route.TRACK_EVENTS
+        )
+
+
     }
 
 }

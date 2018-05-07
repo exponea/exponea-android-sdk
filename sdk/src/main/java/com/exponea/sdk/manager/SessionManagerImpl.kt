bbcd721c05d52f1877f6a4bc2018639ce9c47f12
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-07 15:33:55 +0200

    session timeout implemented

--- sdk/src/main/java/com/exponea/sdk/manager/SessionManagerImpl.kt
+++ sdk/src/main/java/com/exponea/sdk/manager/SessionManagerImpl.kt
@@ -9,42 +9,56 @@ import com.exponea.sdk.preferences.ExponeaPreferences
 import com.exponea.sdk.util.Logger
 import java.util.*
 
-class SessionManagerImpl(val prefs: ExponeaPreferences) : SessionManager {
+class SessionManagerImpl(private val prefs: ExponeaPreferences) : SessionManager {
 
     companion object {
         const val PREF_SESSION_END = "SessionEndTime"
         const val PREF_SESSION_START = "SessionStartTime"
     }
 
+    /**
+     * Calculate session length
+     */
     private fun getSessionLengthInSeconds() : Long {
         val start = prefs.getLong(PREF_SESSION_START, Date().time)
         val end = prefs.getLong(PREF_SESSION_END, Date().time)
+
         return (end - start) / 1000
     }
 
+
     override fun onSessionStart() {
-        Logger.d(this, "Session start")
-        val lastSessionStart = prefs.getLong(PREF_SESSION_START, 0)
-        Logger.d(this, "Last time started: ${Date(lastSessionStart)}")
-        val timestamp = Date().time
-        Logger.d(this, "Now Started: ${Date(timestamp)}")
-        prefs.setLong(PREF_SESSION_START,timestamp)
-        trackStart(timestamp)
+        val now = Date().time
+        Logger.d(this, "Session start ${Date(now)}")
+
+        // Check if current session is the first one
+        val lastTimeStarted = prefs.getLong(PREF_SESSION_START,  -1L)
+        if (lastTimeStarted == -1L) {
+            prefs.setLong(PREF_SESSION_START, now)
+            return
+        }
+
+        if (!canBeResumed(now)) {
+            Logger.d(this, "New Session Started: ${Date(now)}")
+
+            // Finish Tracking old session
+            trackEnd(now)
+
+            // Start Tracking new session
+            prefs.setLong(PREF_SESSION_START,now)
+            trackStart(now)
+        }
+
     }
 
-    override fun onSessionEnd() {
-        Logger.d(this, "Session end")
-        val lastSessionEnd = prefs.getLong(PREF_SESSION_END, 0)
-        Logger.d(this, "Last time end: ${Date(lastSessionEnd)}")
-        val timestamp = Date().time
-        Logger.d(this, "Now Ended: ${Date(timestamp)}")
-        prefs.setLong(PREF_SESSION_END, timestamp)
-        trackEnd(timestamp)
 
+    override fun onSessionEnd() {
+        val now = Date().time
+        Logger.d(this, "Session end ${Date(now)}")
+        prefs.setLong(PREF_SESSION_END, now)
     }
 
     private fun trackStart(timestamp: Long) {
-
         val properties = DeviceProperties().toHashMap()
         properties["app_version"] = BuildConfig.VERSION_CODE
         Exponea.trackEvent(
@@ -59,14 +73,26 @@ class SessionManagerImpl(val prefs: ExponeaPreferences) : SessionManager {
         val properties = DeviceProperties().toHashMap()
         properties["app_version"] = BuildConfig.VERSION_CODE
         properties["duration"] = getSessionLengthInSeconds()
+        Logger.d(this, "Session duration: ${properties["duration"]}")
+
         Exponea.trackEvent(
                 eventType = Constants.EventTypes.sessionEnd,
                 timestamp = timestamp,
                 properties = properties,
                 route = Route.TRACK_EVENTS
         )
+    }
 
+    /**
+     * Determines if current session can be resumed
+     * ( i.e session timeout didn't expire )
+     */
+    private fun canBeResumed(now: Long) : Boolean {
+        val sessionEnded = prefs.getLong(PREF_SESSION_END, -1L)
+        val currentTimeout = ( now - sessionEnded ) / 1000
+        return currentTimeout < Exponea.sessionTimeout
 
     }
+
 
 }

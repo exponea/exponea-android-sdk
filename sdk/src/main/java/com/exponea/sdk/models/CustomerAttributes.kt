74bfe8592d1ef41cf69c580b7a7debc873927acc
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-09 13:38:16 +0200

    Api end point and params namings fixed

--- sdk/src/main/java/com/exponea/sdk/models/CustomerAttributes.kt
+++ sdk/src/main/java/com/exponea/sdk/models/CustomerAttributes.kt
@@ -3,7 +3,7 @@ package com.exponea.sdk.models
 import com.google.gson.annotations.SerializedName
 
 data class CustomerAttributes(
-        @SerializedName("customer_id")
+        @SerializedName("customer_ids")
         var customerId: CustomerIds = CustomerIds(),
         @SerializedName("attributes")
         var attributes: MutableList<HashMap<String, String>>? = null

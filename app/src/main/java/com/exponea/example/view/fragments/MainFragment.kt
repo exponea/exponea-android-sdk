e231c9f7e5e9d2eeb42eb649db1f2def61f0f068
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-10 16:17:32 +0200

    removed old example call

--- app/src/main/java/com/exponea/example/view/fragments/MainFragment.kt
+++ app/src/main/java/com/exponea/example/view/fragments/MainFragment.kt
@@ -1,16 +1,13 @@
 package com.exponea.example.view.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.exponea.example.App
 import com.exponea.example.R
 import com.exponea.example.models.Constants
 import com.exponea.example.view.base.BaseFragment
-import com.exponea.sdk.Exponea
-import com.exponea.sdk.models.CustomerIds
+
 
 class MainFragment : BaseFragment() {
     override fun onCreateView(
@@ -24,17 +21,5 @@ class MainFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         trackPage(Constants.ScreenNames.mainScreen)
-
-        var attrs = mutableListOf(
-                hashMapOf(
-                        Pair("type", "property"),
-                        Pair("property", "first_name")
-                )
-        )
-
-        Exponea.fetchCustomerAttributes(CustomerIds(cookie = App.instance.userIdManager.uniqueUserID), attrs,
-                onFailure = {Log.d("Call", it) },
-                onSuccess = {Log.d("Call", it.toString()) })
-
     }
 }

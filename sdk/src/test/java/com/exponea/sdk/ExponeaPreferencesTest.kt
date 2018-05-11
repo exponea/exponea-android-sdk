d3e7ba9df6aed76e453d03ea861970b5474cdb1e
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-11 15:15:15 +0200

    Exponia preferences unit testing

---
+++ sdk/src/test/java/com/exponea/sdk/ExponeaPreferencesTest.kt
@@ -0,0 +1,40 @@
+package com.exponea.sdk
+
+import com.exponea.sdk.preferences.ExponeaPreferences
+import com.exponea.sdk.preferences.ExponeaPreferencesImpl
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import kotlin.test.assertEquals
+
+@RunWith(RobolectricTestRunner::class)
+class ExponeaPreferencesTest {
+
+    companion object {
+        const val VAL_BOOL = "booleanValue"
+        const val VAL_STRING = "stringValue"
+    }
+
+    lateinit var prefs: ExponeaPreferences
+
+    @Before
+    fun init() {
+        prefs = ExponeaPreferencesImpl(RuntimeEnvironment.application.applicationContext)
+    }
+
+    @Test
+    fun setBoolean_ShouldPass() {
+        val toSet = false
+        prefs.setBoolean(VAL_BOOL, toSet)
+        assertEquals(toSet, prefs.getBoolean(VAL_BOOL, true))
+    }
+
+    @Test
+    fun setString_ShouldPass() {
+        val toSet = "sampleString"
+        prefs.setString(VAL_STRING, toSet)
+        assertEquals(toSet, prefs.getString(VAL_STRING, "wrong one"))
+    }
+}

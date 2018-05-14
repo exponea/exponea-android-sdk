0ff8b6ae961391059c4c282c46450b542b28eb9e
Ricardo Tokashiki <ricardotokashiki@Ricardos-MacBook-Pro.local>
2018-05-14 10:26:39 +0200

    Added GetCustomerIds tests

--- sdk/src/test/java/com/exponea/sdk/LoggerTest.kt
+++ sdk/src/test/java/com/exponea/sdk/LoggerTest.kt
@@ -14,60 +14,60 @@ class LoggerTest {
     @Test
     fun setLoggerLevelToOff_shouldBeEqual5() {
         Logger.level = Logger.Level.OFF
-        assertEquals(Logger.level.value, 5)
+        assertEquals(5, Logger.level.value)
     }
     @Test
     fun setLoggerLevelToOff_shouldBeEqualOff() {
         Logger.level = Logger.Level.OFF
-        assertEquals(Logger.level, Logger.Level.OFF)
+        assertEquals(Logger.Level.OFF, Logger.level)
     }
 
     // Log level ERROR
     @Test
     fun setLoggerLevelToError_shouldBeEqual4() {
         Logger.level = Logger.Level.ERROR
-        assertEquals(Logger.level.value, 4)
+        assertEquals(4, Logger.level.value)
     }
     @Test
     fun setLoggerLevelToError_shouldBeEqualError() {
         Logger.level = Logger.Level.ERROR
-        assertEquals(Logger.level, Logger.Level.ERROR)
+        assertEquals(Logger.Level.ERROR, Logger.level)
     }
 
     // Log level WARN
     @Test
     fun setLoggerLevelToWarning_shouldBeEqual3() {
         Logger.level = Logger.Level.WARN
-        assertEquals(Logger.level.value, 3)
+        assertEquals(3, Logger.level.value)
     }
     @Test
     fun setLoggerLevelToWarning_shouldBeEqualWarn() {
         Logger.level = Logger.Level.WARN
-        assertEquals(Logger.level, Logger.Level.WARN)
+        assertEquals(Logger.Level.WARN, Logger.level)
     }
 
     // Log level INFO
     @Test
     fun setLoggerLevelToInfo_shouldBeEqual2() {
         Logger.level = Logger.Level.INFO
-        assertEquals(Logger.level.value, 2)
+        assertEquals(2, Logger.level.value)
     }
     @Test
     fun setLoggerLevelToInfo_shouldBeEqualInfo() {
         Logger.level = Logger.Level.INFO
-        assertEquals(Logger.level, Logger.Level.INFO)
+        assertEquals(Logger.Level.INFO, Logger.level)
     }
 
     // Log level VERBOSE
     @Test
     fun setLoggerLevelToVerbose_shouldBeEqual1() {
         Logger.level = Logger.Level.VERBOSE
-        assertEquals(Logger.level.value, 1)
+        assertEquals(1, Logger.level.value)
     }
     @Test
     fun setLoggerLevelToVerbose_shouldBeEqualVerbose() {
         Logger.level = Logger.Level.VERBOSE
-        assertEquals(Logger.level, Logger.Level.VERBOSE)
+        assertEquals(Logger.Level.VERBOSE, Logger.level)
     }
 
     // Log level DEBUG
@@ -79,6 +79,6 @@ class LoggerTest {
     @Test
     fun setLoggerLevelToDebug_shouldBeEqualDebug() {
         Logger.level = Logger.Level.DEBUG
-        assertEquals(Logger.level, Logger.Level.DEBUG)
+        assertEquals(Logger.Level.DEBUG, Logger.level)
     }
 }
---
+++ sdk/src/test/java/com/exponea/sdk/manager/ExponeaMockApi.kt
@@ -0,0 +1,33 @@
+package com.exponea.sdk.manager
+
+import com.exponea.sdk.Exponea
+import com.exponea.sdk.models.CustomerAttributeModel
+import com.exponea.sdk.models.CustomerAttributes
+import com.exponea.sdk.models.Result
+import kotlinx.coroutines.experimental.delay
+
+object ExponeaMockApi {
+
+    suspend fun fetchCustomerId(attributes: CustomerAttributes,
+                                onSuccess: (Result<List<CustomerAttributeModel>>) -> Unit,
+                                onFailure: (String) -> Unit) {
+        Exponea.fetchCustomerAttributes(
+                customerAttributes = attributes,
+                onSuccess = onSuccess,
+                onFailure = onFailure
+        )
+        delay(2000L)
+    }
+
+    suspend fun fetchCustomerAttributes(attributes: CustomerAttributes,
+                                        onSuccess: (Result<List<CustomerAttributeModel>>) -> Unit,
+                                        onFailure: (String) -> Unit) {
+        Exponea.fetchCustomerAttributes(
+                customerAttributes = attributes,
+                onSuccess = onSuccess,
+                onFailure = onFailure
+        )
+        delay(2000L)
+    }
+
+}

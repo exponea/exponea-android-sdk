042986e0f74346733062bb393ea405a3d7c0ccc7
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-14 16:15:50 +0200

    added stop session tracking test

--- sdk/src/test/java/com/exponea/sdk/SessionManagerTest.kt
+++ sdk/src/test/java/com/exponea/sdk/SessionManagerTest.kt
@@ -3,7 +3,9 @@ package com.exponea.sdk
 import android.app.Activity
 import com.exponea.sdk.manager.SessionManager
 import com.exponea.sdk.manager.SessionManagerImpl
-import com.exponea.sdk.preferences.ExponeaPreferencesImpl
+import com.exponea.sdk.models.ExponeaConfiguration
+import com.exponea.sdk.models.FlushMode
+import com.exponea.sdk.preferences.ExponeaPreferences
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,14 +20,24 @@ import kotlin.test.assertNotEquals
 class SessionManagerTest {
 
     private lateinit var sm: SessionManager
-    private lateinit var prefs: ExponeaPreferencesImpl
+    private lateinit var prefs: ExponeaPreferences
 
 
     @Before
     fun init () {
         val context = RuntimeEnvironment.application.applicationContext
-        prefs = ExponeaPreferencesImpl(context)
-        sm = SessionManagerImpl(context, prefs)
+        val configuration = ExponeaConfiguration()
+        configuration.baseURL = "url"
+        configuration.projectToken = "projectToken"
+        configuration.authorization = "projectAuthorization"
+
+        Exponea.init(context, configuration)
+
+        sm = Exponea.component.sessionManager
+        prefs = Exponea.component.preferences
+        Exponea.flushMode = FlushMode.MANUAL
+
+
     }
 
     @Test
@@ -38,9 +50,42 @@ class SessionManagerTest {
 
         // App getting focus
         controller.resume()
+
+        // Checking that start timestamp was successfully saved
         assertNotEquals(-1L, prefs.getLong(SessionManagerImpl.PREF_SESSION_START, -1L))
         assert(Date().time >= prefs.getLong(SessionManagerImpl.PREF_SESSION_START, -1L))
         assert(preTime <= prefs.getLong(SessionManagerImpl.PREF_SESSION_START, -1L))
+
+    }
+
+    @Test
+    fun testSessionStop() {
+        val controller = Robolectric.buildActivity(Activity::class.java).create()
+        sm.startSessionListener()
+
+        //App getting focus
+        controller.resume()
+
+        val preTime = Date().time
+
+        // App looses focus
+        controller.pause()
+
+        //Checking that stop timestamp was successfully saved
+        val sessionEndTime = prefs.getLong(SessionManagerImpl.PREF_SESSION_END, -1L)
+
+        assertNotEquals(-1L, sessionEndTime)
+        assert(preTime <= sessionEndTime)
+
+        // User comes back and then leaves
+        controller.resume()
+        controller.pause()
+
+        val newEndTime = prefs.getLong(SessionManagerImpl.PREF_SESSION_END, -1L)
+        assertNotEquals(sessionEndTime, newEndTime)
+        assert(sessionEndTime < newEndTime)
+
+
 
     }
 

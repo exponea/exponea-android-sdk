8986e4cfa858a41853f1a215fa16b5bf9591ea35
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-16 12:55:29 +0200

    added tooblar in main activity

--- app/src/main/java/com/exponea/example/view/MainActivity.kt
+++ app/src/main/java/com/exponea/example/view/MainActivity.kt
@@ -5,14 +5,17 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.exponea.example.R
 import com.exponea.example.models.Constants
-import com.exponea.example.view.fragments.*
 import com.exponea.sdk.Exponea
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_authentication.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.title = "Examples"
 
         if (!Exponea.isInitialized) {
             startActivity(Intent(this, AuthenticationActivity::class.java))

521cb908ac2dffeedd6454bdfe9b59c028126a3a
Roman Levinzon <levinzon.roman@gmail.com>
2018-05-25 16:48:57 +0200

    Commented out lines causing crashes

--- sdk/src/main/java/com/exponea/sdk/services/ExponeaFirebaseInstanceService.kt
+++ sdk/src/main/java/com/exponea/sdk/services/ExponeaFirebaseInstanceService.kt
@@ -6,9 +6,14 @@ import com.google.firebase.iid.FirebaseInstanceIdService
 
 class ExponeaFirebaseInstanceService : FirebaseInstanceIdService() {
     override fun onTokenRefresh() {
+
+        //TODO to fix lateinit not initialized bug
+        /*
         if (!Exponea.isAutoPushNotification) {
             return
         }
+
+        */
 
         Logger.d(this, "Firebase Token Refreshed")
         Exponea.component.pushManager.trackFcmToken()

db4b0e7d7f32e838364a74f3150f02c2221c37e6
Roman Levinzon <levinzon.roman@gmail.com>
2018-06-27 14:33:52 +0200

    Add receiver to sample app to handle notification click

---
+++ app/src/main/java/com/exponea/example/services/MyReceiver.kt
@@ -0,0 +1,27 @@
+package com.exponea.example.services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.exponea.example.view.MainActivity
+import com.exponea.sdk.Exponea
+import com.exponea.sdk.models.NotificationData
+import com.exponea.sdk.services.ExponeaPushReceiver
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when(intent.action) {
+            ExponeaPushReceiver.ACTION_CLICKED -> {
+
+                // Extract payload data
+                val data = intent.getParcelableExtra<NotificationData>(ExponeaPushReceiver.EXTRA_DATA)
+                Log.i("Receiver", "Payload: $data")
+
+                // Act upon push receiving
+                context.startActivity(Intent(context, MainActivity::class.java))
+            }
+        }
+    }
+}

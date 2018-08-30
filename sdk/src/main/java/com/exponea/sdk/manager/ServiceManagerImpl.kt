b2830ce46e9e0be587f0e7eab96f96fa906226b9
lucassales <saless182@gmail.com>
2018-08-30 14:06:46 +0200

    Fixes to make it compatible with lower android versions
    JobScheduler was being a problem for backwards compatibilty, I replaced  it with WorkManager from archtecture compnents which was already in use in the project

--- sdk/src/main/java/com/exponea/sdk/manager/ServiceManagerImpl.kt
+++ sdk/src/main/java/com/exponea/sdk/manager/ServiceManagerImpl.kt
@@ -1,84 +1,37 @@
 package com.exponea.sdk.manager
 
-import android.app.job.JobInfo
-import android.app.job.JobScheduler
-import android.content.ComponentName
-import android.content.Context
-import android.os.Build
+import androidx.work.Constraints
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.NetworkType
+import androidx.work.PeriodicWorkRequest
+import androidx.work.WorkManager
 import com.exponea.sdk.Exponea
 import com.exponea.sdk.services.ExponeaJobService
-import com.exponea.sdk.util.Logger
+import java.util.concurrent.TimeUnit
 
-
-class ServiceManagerImpl(context: Context) : ServiceManager {
-    /**
-     * Id for our job used for starting and canceling the job
-     */
-    private val jobID: Int = 1400
-    /**
-     * Job scheduler service
-     */
-    private val jobScheduler = context.getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
-    /**
-     * Our component name
-     */
-    private val componentName = ComponentName(context, ExponeaJobService::class.java)
+class ServiceManagerImpl : ServiceManager {
 
     override fun start() {
-        Logger.d(this, "Starting Service")
-
-        val period = Exponea.flushPeriod.timeInMillis
-
-        val job = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            JobInfo.Builder(jobID, componentName)
-                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                    .setMinimumLatency(period)
+        val request = PeriodicWorkRequest.Builder(
+            ExponeaJobService::class.java,
+            Exponea.flushPeriod.timeInMillis,
+            TimeUnit.MILLISECONDS
+        )
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
                     .build()
-        } else {
-            JobInfo.Builder(jobID, componentName)
-                    .setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
-                    .setPeriodic(period)
-                    .build()
-        }
+            )
+            .build()
 
-        if (hasPendingJob()) {
-            Logger.d(this, "Job already scheduled")
-            return
-        }
-
-
-        jobScheduler.schedule(job)
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            ExponeaJobService.TAG,
+            ExistingPeriodicWorkPolicy.KEEP,
+            request
+        )
     }
 
     override fun stop() {
-        Logger.d(this, "Stopping Service")
-        jobScheduler.cancel(jobID)
-    }
-
-    private fun hasPendingJob(): Boolean {
-
-        val jobs: MutableList<JobInfo> = try {
-            jobScheduler.allPendingJobs
-        } catch (exception: IllegalArgumentException) {
-            Logger.e(this, "Error obtaining pending jobs", exception)
-            mutableListOf()
-        }
-
-        for (jobInfo in jobs) {
-            if (jobInfo.id == jobID) {
-                Logger.d(
-                        this,
-                        "Pending Job: ${jobInfo.intervalMillis} -> Current ${Exponea.flushPeriod.timeInMillis}"
-                )
-
-                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    jobInfo.minLatencyMillis == Exponea.flushPeriod.timeInMillis
-                } else {
-                    jobInfo.intervalMillis == Exponea.flushPeriod.timeInMillis
-                }
-            }
-        }
-
-        return false
+        WorkManager.getInstance().cancelAllWorkByTag(ExponeaJobService.TAG)
     }
 }
--- sdk/src/main/java/com/exponea/sdk/services/ExponeaJobService.kt
+++ sdk/src/main/java/com/exponea/sdk/services/ExponeaJobService.kt
@@ -1,36 +1,46 @@
 package com.exponea.sdk.services
 
-import android.app.job.JobParameters
-import android.app.job.JobService
+import androidx.work.Worker
 import com.exponea.sdk.Exponea
 import com.exponea.sdk.models.FlushMode
 import com.exponea.sdk.util.Logger
+import java.util.concurrent.CountDownLatch
 
-class ExponeaJobService : JobService() {
-    override fun onStartJob(params: JobParameters): Boolean {
-        Logger.d(this, "onStartJob")
+class ExponeaJobService : Worker() {
 
-        // If our flush mode isn't set to period then we should cancel all future jobs
-        if (Exponea.flushMode != FlushMode.PERIOD) {
-            jobFinished(params, false)
-            return false
-        }
-
-        Exponea.component.flushManager.onFlushFinishListener = {
-            Logger.d(this, "onStartJob -> Finished")
-            // Once our flushing is done we want to tell the system that we finished and we should reschedule
-            jobFinished(params, true)
-        }
-
-        Exponea.component.flushManager.flushData()
-
-        // Return true to let the job know we aren't finished with our work
-
-        return true
+    companion object {
+        const val TAG = "ExponeaJobServiceWork"
     }
 
-    override fun onStopJob(params: JobParameters): Boolean {
+    override fun doWork(): Result {
+        Logger.d(this, "doWork -> Starting...")
+        val countDownLatch = CountDownLatch(1)
+        // If our flush mode isn't set to period then we should cancel all future jobs
+        if (Exponea.flushMode != FlushMode.PERIOD) {
+            return Result.SUCCESS
+        }
+        try {
+            Exponea.component.flushManager.onFlushFinishListener = {
+                // Once our flushing is done we want to tell the system that we finished and we should reschedule
+                countDownLatch.countDown()
+            }
+            Exponea.component.flushManager.flushData()
+            try {
+                countDownLatch.await()
+            } catch (e: InterruptedException) {
+                Logger.e(this, "doWork -> flush was interrupted", e)
+                return Result.FAILURE
+            }
+        } catch (e: UninitializedPropertyAccessException) {
+            return Result.SUCCESS
+        } catch (e: Exception) {
+            return Result.FAILURE
+        }
+        return Result.RETRY
+    }
+
+    override fun onStopped(cancelled: Boolean) {
+        super.onStopped(cancelled)
         Logger.d(this, "onStopJob")
-        return Exponea.flushMode == FlushMode.PERIOD
     }
 }

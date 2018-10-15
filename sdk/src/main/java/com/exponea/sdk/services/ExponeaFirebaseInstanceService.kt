0260c82ce8b61343551651a2ea502e92d68abd87
Tiago Araujo <tiar@nodesagency.com>
2018-10-15 17:16:17 +0200

    Abort token tracking if the Exponea config is null

--- sdk/src/main/java/com/exponea/sdk/services/ExponeaFirebaseInstanceService.kt
+++ sdk/src/main/java/com/exponea/sdk/services/ExponeaFirebaseInstanceService.kt
@@ -1,5 +1,6 @@
 package com.exponea.sdk.services
 
+
 import android.os.Looper
 import com.exponea.sdk.Exponea
 import com.exponea.sdk.repository.ExponeaConfigRepository
@@ -7,14 +8,16 @@ import com.exponea.sdk.util.Logger
 import com.google.firebase.iid.FirebaseInstanceIdService
 
 class ExponeaFirebaseInstanceService : FirebaseInstanceIdService() {
+
     override fun onTokenRefresh() {
 
         if (!Exponea.isInitialized) {
             val config = ExponeaConfigRepository.get(applicationContext)
-            if (config != null) {
-                Looper.prepare()
-                Exponea.init(applicationContext, config)
+            if (config == null){
+                return
             }
+            Looper.prepare()
+            Exponea.init(applicationContext, config)
         }
         if (!Exponea.isAutoPushNotification) {
             return

909744b4c89557f4d9dd16dbf89f4b1639074b0b
Tiago Araujo <tiar@nodesagency.com>
2018-10-16 08:56:01 +0200

    Fixed token crash when firebase is not init

--- sdk/src/main/java/com/exponea/sdk/manager/PushManager.kt
+++ sdk/src/main/java/com/exponea/sdk/manager/PushManager.kt
@@ -3,7 +3,7 @@ package com.exponea.sdk.manager
 import com.exponea.sdk.models.NotificationData
 
 interface PushManager {
-    val fcmToken: String
+    val fcmToken: String?
 
     fun trackFcmToken()
     fun trackDeliveredPush(data: NotificationData? = null)

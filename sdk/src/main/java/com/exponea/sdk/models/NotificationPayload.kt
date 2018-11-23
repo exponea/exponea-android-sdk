ff6c4c0504ac6f9f466f16b60be8cc5640aee37a
Tiago Araujo <taraujodesouza@gmail.com>
2018-11-23 14:50:54 +0100

    Made notification payload code easier to understand

---
+++ sdk/src/main/java/com/exponea/sdk/models/NotificationPayload.kt
@@ -0,0 +1,81 @@
+package com.exponea.sdk.models
+
+import com.exponea.sdk.util.fromJson
+import com.google.gson.Gson
+import org.json.JSONArray
+import java.util.*
+
+@Suppress("ArrayInDataClass")
+data class NotificationPayload(
+        val image: String? = null,
+        val sound: String? = null,
+        val buttons: ArrayList<ActionPayload>? = null,
+        val notificationAction: ActionPayload? = null,
+        val attributes: Map<String, String>? = null
+) {
+
+    /**
+     * Construct the payload using the json data
+     */
+    constructor(map: Map<String, String>) : this(
+            map["image"],
+            map["sound"],
+            handleButtons(map["actions"]),
+            handleNotificationAction(map["action"], map["url"]),
+            handleAttributes(map["attributes"])
+    )
+
+    data class ActionPayload(
+            val action: Actions? = null,
+            val url: String? = null,
+            val title: String? = null
+    )
+
+    companion object {
+
+        /**
+         * Handle the extra attributes that can be send in the notification
+         */
+        private fun handleAttributes(attributeJson: String?): Map<String, String>? {
+            return if (attributeJson == null) null else Gson().fromJson(attributeJson)
+        }
+
+        /**
+         * Create an array with all the buttons action payloads
+         */
+        private fun handleButtons(buttonsJson: String?): ArrayList<ActionPayload>? {
+            if (buttonsJson == null)
+                return null
+
+            val buttonsArray: ArrayList<ActionPayload> = arrayListOf()
+            val buttonsJsonArray = JSONArray(buttonsJson)
+
+            //if we have a button payload, verify each button action
+            for (i in 0 until buttonsJsonArray.length()) {
+                val item: Map<String, String> = Gson().fromJson(buttonsJsonArray[i].toString())
+                val actionEnum = Actions.find(item["action"])
+                buttonsArray.add(ActionPayload(actionEnum, item["url"], item["title"]))
+            }
+
+            return buttonsArray
+        }
+
+        private fun handleNotificationAction(actionString: String?, actionUrl: String?): ActionPayload {
+            return ActionPayload(Actions.find(actionString), actionUrl)
+        }
+    }
+
+    /**
+     * Each action is used to trigger a different event when clicking a notification body or button
+     */
+    enum class Actions(val value: String) {
+        APP("app"),
+        BROWSER("browser"),
+        DEEPLINK("deeplink");
+
+        companion object {
+            fun find(value: String?) = Actions.values().find { it.value == value }
+        }
+    }
+
+}
--- sdk/src/main/java/com/exponea/sdk/services/ExponeaPushReceiver.kt
+++ sdk/src/main/java/com/exponea/sdk/services/ExponeaPushReceiver.kt
@@ -41,33 +41,23 @@ class ExponeaPushReceiver : BroadcastReceiver() {
 
         Logger.i(this, "Push notification clicked")
 
+        val url = intent.getStringExtra(EXTRA_URL)
+        val buttonClickedIntent = Intent(Intent.ACTION_VIEW)
+        buttonClickedIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        if (url != null && url.isNotEmpty())
+            buttonClickedIntent.data = Uri.parse(url)
+
         when (intent.action) {
-            ACTION_CLICKED -> {
-            }
-            ACTION_DEEPLINK_CLICKED -> {
-                val url = intent.getStringExtra(EXTRA_URL)
-                if (url.isNotEmpty()) {
-                    val i = Intent(Intent.ACTION_VIEW).apply {
-                        data = Uri.parse(url)
-                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                    }
-                    PendingIntent.getActivity(context, 0, i, 0).send()
-                }
-            }
-            ACTION_URL_CLICKED -> {
-                val url = intent.getStringExtra(EXTRA_URL)
-                if (url.isNotEmpty()) {
-                    val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                    browserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    context.startActivity(browserIntent)
-                }
-            }
+            ACTION_CLICKED -> Unit
+            ACTION_DEEPLINK_CLICKED -> PendingIntent.getActivity(context, 0, buttonClickedIntent, PendingIntent.FLAG_UPDATE_CURRENT).send()
+            ACTION_URL_CLICKED -> context.startActivity(buttonClickedIntent)
         }
 
         val data = intent.getParcelableExtra(EXTRA_DATA) as NotificationData?
         if (!Exponea.isInitialized) {
             val config = ExponeaConfigRepository.get(context)
-            if (config != null) {
+            config?.let {
                 Logger.d(this, "Newly initiated")
                 Exponea.init(context.applicationContext, config)
             }
@@ -78,12 +68,21 @@ class ExponeaPushReceiver : BroadcastReceiver() {
         )
 
         // After clicking the notification button (action), dismiss it
+        dimissNotification(context, intent)
+
+        // And close the notification tray
+        closeNotificationTray(context)
+    }
+
+    private fun dimissNotification(context: Context, intent: Intent) {
         val notificationId = intent.getIntExtra(EXTRA_NOTIFICATION_ID, -1)
         if (notificationId != -1) {
             val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             manager.cancel(notificationId)
         }
-        // And close the notification tray
+    }
+
+    private fun closeNotificationTray(context: Context) {
         val it = Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS)
         context.sendBroadcast(it)
     }

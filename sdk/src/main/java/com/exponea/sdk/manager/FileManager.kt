a0c35c0f74fa660ae03ba670ca7842056c5c982c
Juraj Dudak <juraj.dudak@panaxeo.com>
2019-10-21 15:24:39 +0000

    fix/Remove dead code from FileManager

--- sdk/src/main/java/com/exponea/sdk/manager/FileManager.kt
+++ sdk/src/main/java/com/exponea/sdk/manager/FileManager.kt
@@ -6,9 +6,5 @@ import com.exponea.sdk.models.ExponeaConfiguration
 interface FileManager {
     fun createFile(filename: String, type: String)
     fun writeToFile(filename: String, text: String)
-    fun readContentFromFile(filename: String): String?
-    fun readContentFromDefaultFile(context: Context): String?
-    fun deleteFile(filename: String)
-    fun getConfigurationFromFile(filename: String): ExponeaConfiguration?
     fun getConfigurationFromDefaultFile(context: Context): ExponeaConfiguration?
 }
